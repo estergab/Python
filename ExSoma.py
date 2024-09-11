75f45c7dccf204feec43e6be5269a1f107d7e671
Ester Gabriela <164204062+estergab@users.noreply.github.com>
2024-09-10 22:00:28 -0300

    UC6 Python - App Desktop

---
+++ ExSoma.py
@@ -0,0 +1,6 @@
+num1= int(input("Vamos fazer uma continha!\nDigite o primeiro número: "))
+num2= int(input("Digite o segundo número: "))
+
+soma= num1 + num2
+
+print(f'A soma dos números resulta em: {soma}')
---
+++ ExWhile.py
@@ -0,0 +1,12 @@
+count = 0
+
+while count < 11:
+    print(f'Contagem: {count}')
+    count += 1
+
+print('--------------------------')
+count = 10
+
+while count > 0:
+    print(f'Contagem: {count}')
+    count -= 1
